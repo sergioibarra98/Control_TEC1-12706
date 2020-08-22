df2600e848728b82bbc19592cb2f1123ea2914db
Sergio Ibarra <sergio.ibarra2398@gmail.com>
2020-08-22 12:00:10 +0200

    Improving sample rating and added contant TEMP_AVG & DESVIATION

--- DataAcquisition/DataAcquisition.ino
+++ DataAcquisition/DataAcquisition.ino
@@ -1,11 +1,15 @@
 #define SENSOR 0          //Pin
-#define N 3               //Average of temperature
+#define N 5               //Average of temperature
+#define TEMP_AVG 112      //22ºC * 255 / 50
+#define DESVIATION 30     //6ΔºC * 255 / 50
 #define ELONGATION 3      //Updating of the output
-#define FRECUENCY 30      //Hz
+#define FRECUENCY 100     //Hz
 #define TIMER 10          //Seconds
 
-int elongation_i = 1, vout[TIMER * FRECUENCY], vread[TIMER * FRECUENCY];
+//Variables
+int elongation_i = 1, vout = TEMP_AVG - DESVIATION, vread;
 
+//Read the temperature N times and return the average
 int readTemp() {
   int temp = 0, temp_k1 = 0;
   for (int i = 0; i < N ; i++) {
@@ -24,43 +28,34 @@ void setup() {
   Serial.print(TIMER * FRECUENCY);
   Serial.print(" Samples | ");
   Serial.print(1 * 1000 / FRECUENCY);
-  Serial.println(" delay");
+  Serial.println(" sampling rate");
 
+  //Generate a PRBS sequence
   for (int ts = 0; ts < TIMER * FRECUENCY; ts++) {
     if (elongation_i == ELONGATION) {
       switch (random(0, 2)) {
-        case 0: vout[ts] = 76; break;
-        case 1: vout[ts] = 179; break;
-        default: vout[ts] = -1; break;
+        case 0: vout = TEMP_AVG - DESVIATION; break;
+        case 1: vout = TEMP_AVG + DESVIATION; break;
+        default: vout = -1; break;
       }
       elongation_i = 0;
-    } else {
-      vout[ts] = vout[ts - 1];
     }
 
     elongation_i++;
 
-    analogWrite(9, vout[ts]);
-    vread[ts] = readTemp();
-    Serial.print(vout[ts]);
+    analogWrite(9, vout);
+    vread = readTemp();
+    Serial.print(vout);
     Serial.print(" ");
-    Serial.println(vread[ts]);
+    Serial.println(vread);
 
     delay(1 * 1000 / FRECUENCY);
   }
 
-  vout[TIMER * FRECUENCY + 1] = 0;
-  analogWrite(9, vout[TIMER * FRECUENCY + 1]);
-  Serial.println(vout[TIMER * FRECUENCY + 1]);
+  vout = 0;
+  analogWrite(9, vout);
+  Serial.println(vout);
   Serial.println("--------------------------------------");
-
-  Serial.print("\n");
-  for (int ts = 0; ts < TIMER * FRECUENCY; ts++) {
-    Serial.print((float)vout[ts] * 5 / 255);
-    Serial.print("V | ");
-    Serial.print(readTemp() * 0.47);
-    Serial.println(" ºC");
-  }
 }
 
 void loop() {
